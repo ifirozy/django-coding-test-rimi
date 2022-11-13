ed1c23bd1cf41bd44f81a1109a18b091cfa6ae86
ifirozy <ifirozy@gmail.com>
2022-11-13 13:03:27 +0000

    data list done

--- src/product/views/product.py
+++ src/product/views/product.py
@@ -1,6 +1,7 @@
 from django.views import generic
-
-from product.models import Variant
+from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from product.models import Variant,Product
 
 
 class CreateProductView(generic.TemplateView):
@@ -12,5 +13,24 @@ class CreateProductView(generic.TemplateView):
         context['product'] = True
         context['variants'] = list(variants.all())
         return context
+
+def productListView(request):
+    products = Product.objects.all()
+    page = request.GET.get('page', 1)
+    query_size = 2
+    paginator = Paginator(products, query_size)
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+    context = {
+        'product': True,
+        'products':products,
+        'query_size':query_size
+    }
+    return render(request,'products/list.html',context)
+
 
 
